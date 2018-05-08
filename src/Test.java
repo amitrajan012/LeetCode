609be34d50762fd0457555a8ac447c254f7b85e6
Amit Rajan <amitrajan012@gmail.com>
2018-05-07 20:13:08 -0400

    May 7:Easy

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,11 @@
-import java.util.HashSet;
-import java.util.Set;
 
 public class Test {
-public static void main(String[] args)  {
-	Set<Character> row1 = new HashSet<Character>() {{
-		add('q');add('w');add('e');add('r');add('t');add('y');add('u');add('i');add('o');add('p');
-	}};
-	System.out.print(row1.contains('q'));
+	public static void main(String[] args)  {
+		StringBuilder sb = new StringBuilder();
+		int num = -7;
+		sb.append(num%7);
+		num /= 7;
+		sb.append(num);
+		System.out.print(sb.toString());
 	}
 }
